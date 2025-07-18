8deff0da1a77813e03565f53daab59ad45232130
Prabhjot Singh <ghaiprabhghai@gmail.com>
2025-07-18 23:55:33 +0530

    Single Factory Method

---
+++ 3. Factory Pattern/SingleFactory.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Burger{
+    public:
+        virtual void prepare() = 0;
+};
+
+class BasicBurger : public Burger{
+   public:
+   void prepare() override {
+       cout << "Basic Burger";
+   }
+};
+class StandardBurger : public Burger{
+   public:
+   void prepare() override {
+       cout << "Standard Burger";
+   }
+ };
+
+ class PremiumBurger : public Burger{
+   public:
+   void prepare() override {
+       cout << "Premium Burger ";
+   }
+ };
+
+ class BurgerFactory{
+    public:
+    Burger* createBurger(string &type)
+    {
+         if(type == "Basic")
+         {
+             return new BasicBurger();
+         }else if(type == "Standard")
+         {
+             return new StandardBurger();
+         }else{
+             return new PremiumBurger();
+         }
+    }
+ };
+
+int main()
+{
+    string type = "Standard";
+    BurgerFactory *obj = new BurgerFactory();
+    Burger* burger = obj->createBurger(type);
+    burger->prepare();
+    return 0;
+}
